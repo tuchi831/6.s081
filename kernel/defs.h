ddcb4614db2ffa9bfff7e26738351e558a685f6d
Tuchi <3277386697@qq.com>
2024-02-16 17:30:42 +0800

    lab3_task2

--- kernel/defs.h
+++ kernel/defs.h
@@ -189,7 +189,7 @@ int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
 void            vmprint(pagetable_t);
-
+// 我这个版本会不会不对
 // plic.c
 void            plicinit(void);
 void            plicinithart(void);
